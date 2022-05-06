b4b3902b838891438c0248841b5f062edbbc7303
Sergio Jurado <sergiojurado.casado@gmail.com>
2022-05-07 00:42:35 +0200

    Unignore public/ folder and regenerate gulp sass compile

--- gulpfile.js
+++ gulpfile.js
@@ -10,59 +10,19 @@ const bsync = require('browser-sync').create();
 const {GULP_ENV} = process.env;
 
 // Converting from .scss to .css:
-function scssToCss() {
+function scss() {
     return src('./src/scss/index.scss')
         .pipe(sourcemaps.init())
-        .pipe(
-            sass({
-                outputStyle: 'expanded',
-            })
-        )
+        .pipe(sass())
         .pipe(prefix())
-        .pipe(minify())
         .pipe(sourcemaps.write('.'))
         .pipe(dest('./public/assets/'));
 }
 
-// minify js
-function jsminify() {
-    return src('./src/main.js')
-        .pipe(sourcemaps.init())
-        .pipe(
-            terser({
-                sourceMap: true,
-                ecma: 6,
-                ie8: true,
-            })
-        )
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('./public/assets/')); // change to your final/public directory
-}
-
-function initServe() {
-    bsync.init({
-        server: {
-            baseDir: './',
-            index: 'index.html',
-        },
-        browser: 'default',
-        cors: true,
-        files: ['./public/**/*.*'],
-        host: 'localhost',
-        injectChanges: true,
-        logLevel: 'info',
-        port: 3000,
-        open: true,
-        notify: true,
-    });
-}
-
 // Watching Mode:
 function watchTask() {
-    watch('./src/scss/**/*.scss', scssToCss);
-    watch('./src/**/*.js', jsminify);
-    watch('./index.html').on('change', bsync.reload);
+    watch('./src/scss/**/*.scss', scss);
 }
 
 // Exec default Gulp Task:
-exports.default = series(parallel(scssToCss, jsminify), initServe, watchTask);
+exports.default = series(parallel(scss), watchTask);
