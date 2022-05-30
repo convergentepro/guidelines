b6c1bb13678cab80a953ddfe677e4f7d0d4f536b
Sergio Jurado <sergiojurado.casado@gmail.com>
2022-05-30 18:48:17 +0200

    Gulp options server and watch. Change commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,94 @@
-const { series, src, dest, watch, task, parallel } = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const postcss = require("gulp-postcss");
-const prefixer = require("autoprefixer");
-const plumber = require('gulp-plumber');
-const { stream, reload } = require( "browser-sync" );
-const babel = require( "gulp-babel" );
-const sourcemaps = require('gulp-sourcemaps');
-const terser = require( "gulp-terser" );
-const concat = require( 'gulp-concat' );
-const tscript = require('gulp-typescript');
-const { nanoid } = require('nanoid');
-const cssnano = require( "cssnano" );
-const copyFiles = require( 'gulp-copy' );
-const fs = require( 'fs/promises' );
-const path = require('path');
-// Gulp Task Functions
-const hasher = nanoid().replace( '-,_', '' );
+const { series, src, dest, watch, parallel } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss')
+const prefixer = require('autoprefixer')
+const plumber = require('gulp-plumber')
+const { stream, reload, init } = require('browser-sync').create()
+const babel = require('gulp-babel')
+const sourcemaps = require('gulp-sourcemaps')
+const terser = require('gulp-terser')
+const concat = require('gulp-concat')
+const tscript = require('gulp-typescript')
+const { nanoid } = require('nanoid')
+const cssnano = require('cssnano')
+const path = require('path')
+
+// ? Settings:
+const hasher = nanoid().replace('-,_', '')
 
 // CSS and SCSS in Development Mode:
-function compilecss(){
-	return src( './src/scss/app.scss')
-		.pipe( plumber() )
+function compilecss() {
+	return src('./src/scss/*.scss')
+		.pipe(plumber())
 		.pipe(sourcemaps.init())
-		.pipe( sass().on( 'error', sass.logError ) )
-		.pipe( postcss( [prefixer()] ) )
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([prefixer()]))
 		.pipe(sourcemaps.write('.'))
-		.pipe( dest( './public/static', { 
-			overwrite: true,
-			
-		} ) )
-		.pipe( stream() );
+		.pipe(dest('./public/static'))
+		.pipe(stream())
 }
 
- 
+// * Typescript Task:
 function compilets() {
-	return src( './src/index.ts' )
-		.pipe( plumber() )
-		.pipe( sourcemaps.init() )
-		.pipe( tscript( { 
-			target: 'es5',
-			module: 'es6',
-			moduleResolution: 'node',
-			sourceMap: true,
-
-		} ) )
-				.pipe( babel( {
-			presets: [
-				"@babel/preset-env"
-			],
-			plugins: [
-				"@babel/plugin-transform-runtime",
-				"@babel/plugin-proposal-class-properties",
-				"@babel/plugin-proposal-function-bind",
-				"@babel/plugin-syntax-dynamic-import"
-			],
-			ignore: ['node_modules'],
-		} ) )
-		.pipe( sourcemaps.write() )
-		.pipe( dest( './public/static', { overwrite: true } ) )
-		.pipe( stream( { match: '**/*.ts' }) );
- }
-
-
- //----------------------------------------------------------------
- // BUILDING TASKS:  
-
-//  Building Css: 
-function cssBuild() {
-	return src( './public/app.css' )
-		.pipe( postcss( [cssnano()] ) )
-		.pipe(concat(`app.${hasher}.css`))
-		.pipe(dest('dist/assets/'))
+	return src('./src/index.ts')
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(
+			tscript({
+				target: 'es5',
+				module: 'es6',
+				moduleResolution: 'node',
+				sourceMap: true,
+				outDir: './public',
+				isolatedModules: true,
+			})
+		)
+		.pipe(
+			babel({
+				presets: ['@babel/preset-env'],
+				plugins: [
+					'@babel/plugin-transform-runtime',
+					'@babel/plugin-proposal-class-properties',
+					'@babel/plugin-proposal-function-bind',
+					'@babel/plugin-syntax-dynamic-import',
+				],
+				ignore: ['node_modules'],
+			})
+		)
+		.pipe(sourcemaps.write())
+		.pipe(dest('./public/static', { overwrite: true }))
+		.pipe(stream())
 }
 
-//  Building Css: 
-function jsBuild() {
-	return src( './public/*.js' )
-		.pipe( terser({ toplevel: true, ie8: true, sourceMap: true}))
-		.pipe(concat(`main.${hasher}.js`))
-		.pipe(dest('dist/assets/'))
-}
-
-// Process Html 
-function compilehtml ()
-{ 
-
-	return fs.copyFile( path.join(__dirname, './src/index.html'), './public/index.html')
+function server() {
+	init({
+		server: {
+			baseDir: './public',
+			// index: 'index.html',
+		},
+		injectChanges: true,
+		ghostMode: true,
+		browser: 'brave browser',
+		watch: true,
+		watchOptions: {
+			depth: 1,
+			persistent: true,
+		},
+		port: 3000,
+		logLevel: 'debug',
+		open: false,
+		notify: true,
+		reloadOnRestart: true,
+	})
 }
 
 // Watcher Gulp Task
 function watching() {
-	watch("./src/scss/**/*.scss", compilecss);
-	watch("./src/*.{js,ts}", compilets).on("change", reload);
-	watch("./*.html", compilehtml).on("change", reload);
+	watch('./src/scss/**/*.scss', compilecss)
+	watch(['./src/*.ts'], compilets).on('change', reload)
+	watch('./public/*.html').on('change', reload)
 }
 
-
-
-exports.html = compilehtml;
-exports.ts = compilets;
-exports.build = series(cssBuild, jsBuild);
-exports.default = series(compilecss, compilets, compilehtml);
+// ? Exports:
+exports.serve = server
+exports.default = series(parallel(compilecss, compilets), watching)
